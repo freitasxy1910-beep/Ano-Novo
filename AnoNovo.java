063b89a61bc6590b235e9cabbddbde686871f882
freitasxy1910-beep <freitas.xy1910@gmail.com>
2026-09-25 07:28:56 -0300

    Add files via upload

---
+++ AnoNovo.java
@@ -0,0 +1,9 @@
+public class AnoNovo {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("FELIZ ANO NOVO!");
+    }
+}
